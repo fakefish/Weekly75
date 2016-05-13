c1281f450e1d1bb2acd7ef3beebadb0d2dac3637
王佳裕 <fakefish@wangjiayudeMacBook-Pro.local>
2016-05-13 13:18:04 +0800

    update localhost

--- ios/Weekly75/AppDelegate.m
+++ ios/Weekly75/AppDelegate.m
@@ -34,7 +34,7 @@
    * on the same Wi-Fi network.
    */
 
-   jsCodeLocation = [NSURL URLWithString:@"http://10.18.61.49:8081/index.ios.bundle?platform=ios&dev=true"];
+   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
